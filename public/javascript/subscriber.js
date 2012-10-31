216df2c2cad6432f36d1edcc75930fd21d5bf4d3
hussainSynerzip <hussain.pithawala@synerzip.com>
2012-10-31 15:05:26 +0530

    Many changes

---
+++ public/javascript/subscriber.js
@@ -0,0 +1,9 @@
+/*!
+ * Copyright Quickoffice, Inc, 2005-2011
+ *
+ * NOTICE:   The intellectual and technical concepts contained
+ * herein are proprietary to Quickoffice, Inc. and is protected by
+ * trade secret and copyright law. Dissemination of any of this
+ * information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from Quickoffice, Inc.
+ */
